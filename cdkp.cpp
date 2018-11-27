47ad3485e1ec777778be9dac25147ef55fc460e7
Gerald <gerald.schade@gmx.de>
2018-11-27 21:35:50 +0100

    aus Makefile

--- cdkp.cpp
+++ cdkp.cpp
@@ -2,15 +2,42 @@
 #include <vector>
 using namespace std;
 
-vector<CDKOP*> all_objects;
+vector<CDKOBJS*> all_objects;
 
+/*
+ * Return an integer like 'floor()', which returns a double.
+ */
+int floorCDK (double value)
+{
+   int result = (int)value;
+   if (result > value)		/* e.g., value < 0.0 and value is not an integer */
+      result--;
+   return result;
+}
+
+/*
+ * Return an integer like 'ceil()', which returns a double.
+ */
+int ceilCDK (double value)
+{
+   return -floorCDK (-value);
+}
+
+/*
+ * This beeps then flushes the stdout stream.
+ */
+void Beep (void)
+{
+   beep ();
+   fflush (stdout);
+}
 
 
 /*
  * Create a new object beginning with a CDKOBJS struct.  The whole object is
  * initialized to zeroes except for special cases which have known values.
  */
-CDKOP::CDKOP() // void *_newCDKObject (unsigned size, const CDKFUNCS * funcs)
+CDKOBJS::CDKOBJS() // void *_newCDKObject(unsigned size, const CDKFUNCS * funcs)
 {
 	 hasFocus = TRUE;
 	 isVisible = TRUE;
@@ -29,4 +56,329 @@ CDKOP::CDKOP() // void *_newCDKObject (unsigned size, const CDKFUNCS * funcs)
 	 /* set default exit-types */
 	 exitType = vNEVER_ACTIVATED;
 	 earlyExit = vNEVER_ACTIVATED;
+}
+
+/*
+ * This creates a pointer to an entry widget.
+ */
+SEntry::SEntry (CDKSCREEN *cdkscreen,
+		int xplace,
+		int yplace,
+		const char *title,
+		const char *label,
+		chtype fieldAttr,
+		chtype filler,
+		EDisplayType dispType,
+		int fWidth,
+		int min,
+		int max,
+		boolean Box,
+		boolean shadow,
+		// GSchade Anfang
+		int highnr/*=0*/
+		// GSchade Ende
+		)
+{
+	/* *INDENT-EQLS* */
+	CDKENTRY *entry      = 0;
+	int parentWidth      = getmaxx (cdkscreen->window);
+	int parentHeight     = getmaxy (cdkscreen->window);
+	int fieldWidth       = fWidth;
+	int boxWidth         = 0;
+	int boxHeight;
+	int xpos             = xplace;
+	int ypos             = yplace;
+	int junk             = 0;
+	int horizontalAdjust, oldWidth;
+
+	if ((entry = newCDKObject (CDKENTRY, &my_funcs)) == 0)
+		return (0);
+
+	setCDKEntryBox (entry, Box);
+	boxHeight = (BorderOf (entry) * 2) + 1;
+
+	/*
+	 * If the fieldWidth is a negative value, the fieldWidth will
+	 * be COLS-fieldWidth, otherwise, the fieldWidth will be the
+	 * given width.
+	 */
+	fieldWidth = setWidgetDimension (parentWidth, fieldWidth, 0);
+	boxWidth = fieldWidth + 2 * BorderOf (entry);
+
+	/* Set some basic values of the entry field. */
+	entry->label = 0;
+	entry->labelLen = 0;
+	entry->labelWin = 0;
+	entry->labelumlz=0; // GSchade
+
+	// GSchade
+
+	/* Translate the label char *pointer to a chtype pointer. */
+	if (label != 0)
+	{
+
+		entry->label = char2Chtypeh(label, &entry->labelLen, &junk
+				// GSchade Anfang
+				,highnr
+				// GSchade Ende
+				);
+		// GSchade Anfang
+		for(int i=0;entry->label[i];i++) {
+			if ((int)CharOf(entry->label[i])==194 || (int)CharOf(entry->label[i])==195) {
+				entry->labelumlz++;
+			}
+		}
+		// GSchade Ende
+		boxWidth += entry->labelLen;
+	}
+
+	oldWidth = boxWidth;
+	boxWidth = setCdkTitle (ObjOf (entry), title, boxWidth);
+	horizontalAdjust = (boxWidth - oldWidth) / 2;
+
+	boxHeight += TitleLinesOf (entry);
+
+	/*
+	 * Make sure we didn't extend beyond the dimensions of the window.
+	 */
+	boxWidth = MINIMUM (boxWidth, parentWidth);
+	boxHeight = MINIMUM (boxHeight, parentHeight);
+	fieldWidth = MINIMUM (fieldWidth,
+			boxWidth - entry->labelLen +entry->labelumlz - 2 * BorderOf (entry));
+
+	/* Rejustify the x and y positions if we need to. */
+	alignxy (cdkscreen->window, &xpos, &ypos, boxWidth, boxHeight);
+
+	/* Make the label window. */
+	entry->win = newwin (boxHeight, boxWidth, ypos, xpos);
+	if (entry->win == 0)
+	{
+		destroyCDKObject (entry);
+		return (0);
+	}
+	keypad (entry->win, TRUE);
+
+	/* Make the field window. */
+	entry->fieldWin = subwin (entry->win, 1, fieldWidth,
+			(ypos + TitleLinesOf (entry) + BorderOf (entry)),
+			(xpos + entry->labelLen -entry->labelumlz
+			 + horizontalAdjust
+			 + BorderOf (entry)));
+	if (entry->fieldWin == 0)
+	{
+		destroyCDKObject (entry);
+		return (0);
+	}
+	keypad (entry->fieldWin, TRUE);
+
+	/* Make the label win, if we need to. */
+	if (label != 0)
+	{
+		entry->labelWin = subwin (entry->win, 1, entry->labelLen,
+				ypos + TitleLinesOf (entry) + BorderOf (entry),
+				xpos + horizontalAdjust + BorderOf (entry));
+	}
+
+	/* Make room for the info char * pointer. */
+	entry->info = typeMallocN (char, max + 3);
+	if (entry->info == 0)
+	{
+		destroyCDKObject (entry);
+		return (0);
+	}
+	cleanChar (entry->info, max + 3, '\0');
+	entry->infoWidth = max + 3;
+
+	/* *INDENT-EQLS* Set up the rest of the structure. */
+	ScreenOf (entry)             = cdkscreen;
+	entry->parent                = cdkscreen->window;
+	entry->shadowWin             = 0;
+	entry->fieldAttr             = fieldAttr;
+	entry->fieldWidth            = fieldWidth;
+	entry->filler                = filler;
+	entry->hidden                = filler;
+	ObjOf (entry)->inputWindow   = entry->fieldWin;
+	ObjOf (entry)->acceptsFocus  = TRUE;
+	ReturnOf (entry)             = NULL;
+	entry->shadow                = shadow;
+	entry->screenCol             = 0;
+  entry->sbuch=0;
+	entry->leftChar              = 0;
+  entry->lbuch=0;
+	entry->min                   = min;
+	entry->max                   = max;
+	entry->boxWidth              = boxWidth;
+	entry->boxHeight             = boxHeight;
+	initExitType (entry);
+	entry->dispType              = dispType;
+	entry->callbackfn            = CDKEntryCallBack;
+
+	/* Do we want a shadow? */
+	if (shadow)
+	{
+		entry->shadowWin = newwin (
+				boxHeight,
+				boxWidth,
+				ypos + 1,
+				xpos + 1);
+	}
+
+	registerCDKObject (cdkscreen, vENTRY, entry);
+
+	return (entry);
+}
+
+
+void SScroller::scroller_KEY_UP()
+{
+   if (listSize <= 0 || currentItem <= 0) {
+      Beep();
+      return;
+   }
+   currentItem--;
+   if (currentHigh) {
+      currentHigh--;
+   }
+   if (currentTop && currentItem < currentTop) {
+      currentTop--;
+   }
+}
+
+void SScroller::scroller_KEY_DOWN()
+{
+   if (listSize <= 0 || currentItem >= lastItem) {
+      Beep();
+      return;
+   }
+   currentItem++;
+   if(currentHigh < viewSize - 1) {
+      currentHigh++;
+   }
+   if(currentTop < maxTopItem && currentItem >(currentTop + viewSize - 1)) {
+      currentTop++;
+   }
+}
+
+void SScroller::scroller_KEY_LEFT()
+{
+   if (listSize <= 0 || leftChar <= 0) {
+      Beep();
+      return;
+   }
+   leftChar--;
+}
+
+void SScroller::scroller_KEY_RIGHT()
+{
+   if (listSize <= 0 || leftChar >= maxLeftChar) {
+      Beep();
+      return;
+   }
+   leftChar++;
+}
+
+void SScroller::scroller_KEY_PPAGE()
+{
+   int viewSize = viewSize - 1;
+   if (listSize <= 0 || currentTop <= 0) {
+      Beep();
+      return;
+   }
+   if (currentTop < viewSize) {
+      scroller_KEY_HOME();
+   } else {
+      currentTop -= viewSize;
+      currentItem -= viewSize;
+   }
+}
+
+void SScroller::scroller_KEY_NPAGE()
+{
+   int viewSize = viewSize - 1;
+   if (listSize <= 0 || currentTop >= maxTopItem) {
+      Beep();
+      return;
+   }
+   if ((currentTop + viewSize) <= maxTopItem) {
+      currentTop += viewSize;
+      currentItem += viewSize;
+   } else {
+      scroller_KEY_END();
+   }
+}
+
+void SScroller::scroller_KEY_HOME()
+{
+   currentTop = 0;
+   currentItem = 0;
+   currentHigh = 0;
+}
+
+void SScroller::scroller_KEY_END()
+{
+   currentTop = maxTopItem;
+   currentItem = lastItem;
+   currentHigh = viewSize - 1;
+}
+
+void SScroller::scroller_FixCursorPosition()
+{
+   int scrollbarAdj = (scrollbarPlacement == LEFT) ? 1 : 0;
+   int ypos = SCREEN_YPOS(this,currentItem - currentTop);
+   int xpos = SCREEN_XPOS(this,0) + scrollbarAdj;
+   wmove(InputWindowOf(this), ypos, xpos);
+   wrefresh(InputWindowOf(this));
+}
+
+void SScroller::scroller_SetPosition(int item)
+{
+   /* item out of band */
+   if (item <= 0) {
+      scroller_KEY_HOME();
+      return;
+   }
+   /* item out of band */
+   if (item >= lastItem) {
+      scroller_KEY_END();
+      return;
+   }
+   /* item in first view port */
+   if (item < viewSize) {
+      currentTop = 0;
+   }
+   /* item in last view port */
+   else if (item >= lastItem - viewSize) {
+      currentTop = maxTopItem;
+   }
+   /* item not in visible view port */
+   else if (item < currentTop || item >= currentTop + viewSize) {
+      currentTop = item;
+   }
+   currentItem = item;
+   currentHigh = currentItem - currentTop;
+}
+
+int SScroller::scroller_MaxViewSize()
+{
+   return(boxHeight - (2 * BorderOf(this) + TitleLinesOf(this)));
+}
+
+void SScroller::scroller_SetViewSize(int size)
+{
+   int max_view_size = scroller_MaxViewSize();
+   viewSize = max_view_size;
+   listSize = size;
+   lastItem = size - 1;
+   maxTopItem = size - viewSize;
+   if (size < viewSize) {
+      viewSize = size;
+      maxTopItem = 0;
+   }
+   if (listSize > 0 && max_view_size > 0) {
+      step = (float)(max_view_size /(double)listSize);
+      toggleSize =((listSize > max_view_size) ? 1 : ceilCDK(step));
+   } else {
+      step = 1;
+      toggleSize = 1;
+   }
 }
--- cdkp.h
+++ cdkp.h
@@ -16,7 +16,108 @@
 #include <curses.h>
 #endif
 
-struct CDKOP; // CDKOBJS
+/*
+ * Declare miscellaneous defines.
+ */
+#define	LEFT		9000
+#define	RIGHT		9001
+#define	CENTER		9002
+#define	TOP		9003
+#define	BOTTOM		9004
+#define	HORIZONTAL	9005
+#define	VERTICAL	9006
+#define	FULL		9007
+
+#define NONE		0
+#define ROW		1
+#define COL		2
+
+#define MAX_BINDINGS	300	/* unused by widgets */
+#define MAX_ITEMS	2000	/* unused by widgets */
+#define MAX_BUTTONS	200	/* unused by widgets */
+
+#define	MAXIMUM(a,b)	((a) > (b) ? (a) : (b))
+#define	MINIMUM(a,b)	((a) < (b) ? (a) : (b))
+#define	HALF(a)		((a) >> 1)
+
+#ifndef COLOR_PAIR
+#define	COLOR_PAIR(a)	A_NORMAL
+#endif
+
+#define ObjOf(ptr)              (&(ptr)->obj)
+#define MethodOf(ptr)           (ObjOf(ptr)->fn)
+#define ScreenOf(ptr)           (ObjOf(ptr)->screen)
+#define WindowOf(ptr)           (ScreenOf(ptr)->window)
+#define BorderOf(p)             (ObjOf(p)->borderSize)
+#define ResultOf(p)             (ObjOf(p)->resultData)
+#define ExitTypeOf(p)           (ObjOf(p)->exitType)
+#define EarlyExitOf(p)          (ObjOf(p)->earlyExit)
+
+/* titles */
+#define TitleOf(w)              ObjOf(w)->title
+#define TitlePosOf(w)           ObjOf(w)->titlePos
+#define TitleLenOf(w)           ObjOf(w)->titleLen
+#define TitleLinesOf(w)         ObjOf(w)->titleLines
+
+/* line-drawing characters */
+#define ULCharOf(w)             ObjOf(w)->ULChar
+#define URCharOf(w)             ObjOf(w)->URChar
+#define LLCharOf(w)             ObjOf(w)->LLChar
+#define LRCharOf(w)             ObjOf(w)->LRChar
+#define VTCharOf(w)             ObjOf(w)->VTChar
+#define HZCharOf(w)             ObjOf(w)->HZChar
+#define BXAttrOf(w)             ObjOf(w)->BXAttr
+
+#define setULCharOf(o,c)        MethodOf(o)->setULcharObj(ObjOf(o),c)
+#define setURCharOf(o,c)        MethodOf(o)->setURcharObj(ObjOf(o),c)
+#define setLLCharOf(o,c)        MethodOf(o)->setLLcharObj(ObjOf(o),c)
+#define setLRCharOf(o,c)        MethodOf(o)->setLRcharObj(ObjOf(o),c)
+#define setVTCharOf(o,c)        MethodOf(o)->setVTcharObj(ObjOf(o),c)
+#define setHZCharOf(o,c)        MethodOf(o)->setHZcharObj(ObjOf(o),c)
+#define setBXAttrOf(o,c)        MethodOf(o)->setBXattrObj(ObjOf(o),c)
+#define setBKAttrOf(o,c)        MethodOf(o)->setBKattrObj(ObjOf(o),c)
+
+   /* pre/post-processing */
+#define PreProcessFuncOf(w)	(ObjOf(w)->preProcessFunction)
+#define PreProcessDataOf(w)	(ObjOf(w)->preProcessData)
+#define PostProcessFuncOf(w)	(ObjOf(w)->postProcessFunction)
+#define PostProcessDataOf(w)	(ObjOf(w)->postProcessData)
+/*
+ * Position within the data area of a widget, accounting for border and title.
+ */
+#define SCREEN_XPOS(w,n) ((n) + BorderOf(w))
+#define SCREEN_YPOS(w,n) ((n) + BorderOf(w) + TitleLinesOf(w))
+
+/* The cast is needed because traverse.c wants to use CDKOBJS pointers */
+#define ObjPtr(p)           ((CDKOBJS*)(p))
+
+#define MethodPtr(p,m)      ((ObjPtr(p))->fn->m)
+
+/* Use these when we're certain it is a CDKOBJS pointer */
+#define ObjTypeOf(p)            MethodPtr(p,objectType)
+#define DataTypeOf(p)           MethodPtr(p,returnType)
+#define DrawObj(p)              MethodPtr(p,drawObj)         (p,p->box)
+#define EraseObj(p)             MethodPtr(p,eraseObj)        (p)
+#define DestroyObj(p)           MethodPtr(p,destroyObj)      (p)
+#define InjectObj(p,k)          MethodPtr(p,injectObj)       (p,(k))
+#define InputWindowObj(p)       MethodPtr(p,inputWindowObj)  (p)
+#define FocusObj(p)             MethodPtr(p,focusObj)        (p)
+#define UnfocusObj(p)           MethodPtr(p,unfocusObj)      (p)
+#define SaveDataObj(p)          MethodPtr(p,saveDataObj)     (p)
+#define RefreshDataObj(p)       MethodPtr(p,refreshDataObj)  (p)
+#define SetBackAttrObj(p,c)     MethodPtr(p,setBKattrObj)    (p,c)
+
+#define AcceptsFocusObj(p)      (ObjPtr(p)->acceptsFocus)
+#define HasFocusObj(p)          (ObjPtr(p)->hasFocus)
+#define IsVisibleObj(p)         (ObjPtr(p)->isVisible)
+#define InputWindowOf(p)        (ObjPtr(p)->inputWindow)
+
+void Beep();
+int floorCDK (double value);
+int ceilCDK (double value);
+
+
+struct CDKOBJS; // CDKOBJS
 
 typedef enum {
       CDKSCREEN_NOEXIT = 0
@@ -68,6 +169,28 @@ typedef enum {	vNULL = 0
 		} EObjectType;
 
 /*
+ * This enumerated typedef lists all the valid display types for
+ * the entry, mentry, and template widgets.
+ */
+typedef enum {	vINVALID = 0
+		,vCHAR
+		,vHCHAR
+		,vINT
+		,vHINT
+		,vMIXED
+		,vHMIXED
+		,vUCHAR
+		,vLCHAR
+		,vUHCHAR
+		,vLHCHAR
+		,vUMIXED
+		,vLMIXED
+		,vUHMIXED
+		,vLHMIXED
+		,vVIEWONLY
+		} EDisplayType;
+
+/*
  * This is the prototype for the process callback functions.
  */
 typedef int (*PROCESSFN) (
@@ -101,7 +224,7 @@ struct CDKOBJS;
  */
 struct SSCREENP { // SScreen
    WINDOW *		window;
-   CDKOP**	object; // CDKOBJS
+   CDKOBJS**	object; // CDKOBJS
    int			objectCount;	/* last-used index in object[] */
    int			objectLimit;	/* sizeof(object[]) */
    EExitStatus		exitStatus;
@@ -120,7 +243,7 @@ typedef enum {vEARLY_EXIT, vESCAPE_HIT, vNORMAL, vNEVER_ACTIVATED, vERROR} EExit
  * each widget's struct to allow us to use generic functions in binding.c,
  * cdkscreen.c, position.c, etc.
  */
-struct CDKOP { // CDKOBJS
+struct CDKOBJS { // CDKOBJS
    int          screenIndex;
    SSCREENP *  screen;
    //const CDKFUNCS * fn;
@@ -150,7 +273,6 @@ struct CDKOP { // CDKOBJS
    /* events */
    EExitType	exitType;
    EExitType	earlyExit;
-   /* pre/post-processing */
    PROCESSFN	preProcessFunction;
    void *	preProcessData;
    PROCESSFN	postProcessFunction;
@@ -176,5 +298,120 @@ struct CDKOP { // CDKOBJS
 	 virtual void setBXattrObj(chtype);
    /* background attribute */
 	 virtual void setBKattrObj(chtype);
-	 CDKOP();
+	 CDKOBJS();
+}; // struct CDKOBJS
+
+/*
+ * Define the CDK entry widget structure.
+ */
+typedef struct SEntry CDKENTRY;
+typedef void (*ENTRYCB) (struct SEntry *entry, chtype character);
+
+struct SEntry {
+   CDKOBJS	obj;
+   WINDOW *	parent;
+   WINDOW *	win;
+   WINDOW *	shadowWin;
+   WINDOW *	labelWin;
+   WINDOW *	fieldWin;
+   chtype *	label;
+   int		labelLen;
+	 int		labelumlz; // GSchade
+   int		titleAdj;
+   chtype	fieldAttr;
+   int		fieldWidth;
+   char *	info;
+   int		infoWidth;
+   int		screenCol;
+   int    sbuch; // GSchade
+   int		leftChar;
+   int    lbuch; // GSchade
+   int		min;
+   int		max;
+   int		boxWidth;
+   int		boxHeight;
+   void settoend(); // GSchade
+   void schreibl(chtype); // GSchade, callbackfn
+   void zeichneFeld(); // GSchade
+   EExitType	exitType;
+   EDisplayType dispType;
+   bool	shadow;
+   chtype	filler;
+   chtype	hidden;
+   ENTRYCB	callbackfn;
+   void		*callbackData;
+	 /*
+		* This creates a pointer to a new CDK entry widget.
+		*/
+	 SEntry(
+			 CDKSCREEN *	/* cdkscreen */,
+			 int		/* xpos */,
+			 int		/* ypos */,
+			 const char *	/* title */,
+			 const char *	/* label */,
+			 chtype		/* fieldAttrib */,
+			 chtype		/* filler */,
+			 EDisplayType	/* disptype */,
+			 int		/* fieldWidth */,
+			 int		/* min */,
+			 int		/* max */,
+			 bool   /* Box */,
+			 bool		/* shadow */,
+			 // GSchade 17.11.18
+			 int highnr/*=0*/
+			 // Ende GSchade 17.11.18
+			 );
+
 };
+
+
+struct SScroller {
+	/* This field must stay on top */
+	CDKOBJS  obj; 
+	WINDOW * parent; 
+	WINDOW * win; 
+	WINDOW * scrollbarWin; 
+	WINDOW * shadowWin; 
+	int      titleAdj;   /* unused */ 
+	chtype **    item; 
+	int *    itemLen; 
+	int *    itemPos; 
+
+	int      currentTop; 
+	int      currentItem; 
+	int      currentHigh; 
+
+	int      maxTopItem; 
+	int      maxLeftChar; 
+	int      maxchoicelen; 
+	int      leftChar; 
+	int      lastItem; 
+	int      listSize; 
+	int      boxWidth; 
+	int      boxHeight; 
+	int      viewSize; 
+
+	int      scrollbarPlacement; 
+	bool  scrollbar; 
+	int      toggleSize; /* size of scrollbar thumb/toggle */ 
+	int      togglePos; /* position of scrollbar thumb/toggle */ 
+	float    step; /* increment for scrollbar */ 
+
+	EExitType    exitType; 
+	bool  shadow; 
+	chtype   highlight;
+	void scroller_KEY_UP();
+	void scroller_KEY_DOWN();
+	void scroller_KEY_LEFT();
+	void scroller_KEY_RIGHT();
+	void scroller_KEY_PPAGE();
+	void scroller_KEY_NPAGE();
+	void scroller_KEY_HOME();
+	void scroller_KEY_END();
+	void scroller_FixCursorPosition();
+	void scroller_SetPosition(int item);
+	int scroller_MaxViewSize();
+	void scroller_SetViewSize(int size);
+};
+typedef struct SScroller CDKSCROLLER;
+
